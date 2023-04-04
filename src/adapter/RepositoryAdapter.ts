90c694e7ab7dc2600096c30255b9796ee737cee5
ThomasA98 <andres.koczan@gmail.com>
2023-04-04 14:49:33 -0400

    D4-M4-A2023 direccion de svg arreglada

--- src/adapter/RepositoryAdapter.ts
+++ src/adapter/RepositoryAdapter.ts
@@ -1,9 +1,12 @@
 import type { Repository } from "../model/Repository";
 
+// site: 'https://ThomasA98.github.io'
+// base: '/portafolio-web'
+
 export const repositoryAdapter = (repos: Array<Repository>) => {
     return repos.map(repo => ({
         ...repo,
         contents_url: repo.contents_url.replace('{+path}', 'README.md'),
-        language: `image/${repo.language?.toLowerCase()}.svg`
+        language: `https://ThomasA98.github.io/portafolio-web/image/${repo.language?.toLowerCase()}.svg`
     }))
 };
